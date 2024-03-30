d5f578c80a24282514908df6e3f25426dff355c8
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2024-03-30 12:39:19 +0330

    creating header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,32 @@
-import ThemeToggler from "./ThemeToggler"
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+// components
+import Logo from "./Logo";
+import ThemeToggler from "./ThemeToggler";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header>
-        <ThemeToggler />
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div className="flex items-center gap-x-6">
+            {/* nav */}
+            <Nav />
+            <ThemeToggler />
+            {/* mobile nav */}
+            <div className="xl:hidden">
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
