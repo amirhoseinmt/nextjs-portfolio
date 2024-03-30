71a98e9fbe026775bc09fb947d934b780443479a
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2024-03-30 13:47:11 +0330

    logo design

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const Logo = () => {
   return (
-    <div>Logo</div>
-  )
-}
+    <Link href="/">
+      <Image src="/logo.png" width={54} height={54} priority alt="" />
+    </Link>
+  );
+};
 
-export default Logo
+export default Logo;
