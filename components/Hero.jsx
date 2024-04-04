a54f88a95eceaf2b7a81d955f9c5926da34a0af5
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2024-04-04 13:42:22 +0330

    hero socials and image

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -30,23 +30,37 @@ const Hero = () => {
               Breif description whit insghts into myself, my vocalional journy,
               and what i engage in professionally.
             </p>
-            {/* button */}
+            {/* buttons */}
             <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
               <Link href="/contact">
                 <Button className="gap-x-2">
                   Contact me <Send size={18} />
                 </Button>
               </Link>
-              <Button variant='secondary' className="gap-x-2">
+              <Button variant="secondary" className="gap-x-2">
                 Download CV <Download size={18} />
               </Button>
             </div>
+            {/* socials */}
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+              iconsStyles="text-foreground text-[22px] hover:text-primary transition-all"
+            />
           </div>
           {/* image */}
-          <div className="hidden xl:flex relative">image</div>
+          <div className="hidden xl:flex relative">
+            <div
+              className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px]
+             bg-no-repeat absolute -top-10 -right-2"
+            ></div>
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc="/hero/developer.png"
+            />
+          </div>
         </div>
         {/* icon */}
-        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+        <div className="hidden md:flex absolute left-2/4 bottom-5 xl:bottom-12 animate-bounce">
           <RiArrowDownSLine className="text-3xl text-primary" />
         </div>
       </div>
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client";
 
-const Socials = () => {
+import {
+  RiInstagramFill,
+  RiTelegramFill,
+  RiLinkedinFill,
+  RiGithubFill,
+} from "react-icons/ri";
+
+import Link from "next/link";
+
+const icons = [
+  {
+    path: '/',
+    name: <RiInstagramFill />,
+  },
+  {
+    path: '/',
+    name: <RiTelegramFill />,
+  },
+  {
+    path: '/',
+    name: <RiLinkedinFill />,
+  },
+  {
+    path: '/',
+    name: <RiGithubFill />,
+  },
+]
+
+const Socials = ({containerStyles, iconsStyles}) => {
   return (
-    <div>Socials</div>
-  )
-}
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => {
+        return (
+          <Link href={icon.path} key={index}>
+            <div className={`${iconsStyles}`}>{icon.name}</div>
+          </Link>
+        )
+      })}
+    </div>
+  );
+};
 
-export default Socials
+export default Socials;
