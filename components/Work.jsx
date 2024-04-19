d4e287f81ae46440b4af7599a4000d8be492f3b0
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2024-04-19 12:53:30 +0330

    reviews component

--- components/Work.jsx
+++ components/Work.jsx
@@ -10,7 +10,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
-import { Pagination, pagination } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 
 //components
 import ProjectCard from "@/components/ProjectCard";
@@ -101,7 +101,7 @@ const projectData = [
 
 const Work = () => {
   return (
-    <section className="relative mb-12 xl:mb-48">
+    <section className="relative mb-32 xl:mb-52">
       <div className="container mx-auto">
         {/* text */}
         <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
